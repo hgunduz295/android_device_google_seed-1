309dc47fd3ee6f7f88a5578b4d6d83ac14951866
Arvin Quilao <arquilao@gmail.com>
2016-06-06 10:02:56 +0800

    Import ro.product.device value from OEM partition

--- device.mk
+++ device.mk
@@ -25,6 +25,7 @@ PRODUCT_COPY_FILES := \
 
 # Delegation for OEM customization
 PRODUCT_SYSTEM_PROPERTY_BLACKLIST := \
+    ro.product.device \
     ro.product.name \
     ro.product.manufacturer \
     ro.product.model
