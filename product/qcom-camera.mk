bb713aae7a5679335c97f68d1206c239631e687b
nicknitewolf <nicknitewolf@lineageos.org>
2017-09-06 20:54:48 +0800

    seed: Use lineage custom legacy camera provider
    * Camera HALs before Treble used to pass camera id
      in user field, in Treble they are meant to pass
      pointer to CameraDevice.
    * This patch workarounds this by storing local pointer
      to CameraDevice and not relying on user field.
    
    Change-Id: I6a26f8f9a3870d4b2aefad86307da036a8f1996c
    
    Conflicts:
    	product/qcom-camera.mk

--- product/qcom-camera.mk
+++ product/qcom-camera.mk
@@ -1,7 +1,7 @@
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl \
-    camera.device@3.2-impl \
+    android.hardware.camera.provider@2.4-impl-legacy \
+    camera.device@1.0-impl-legacy \
     Snap
 
 # Permissions
